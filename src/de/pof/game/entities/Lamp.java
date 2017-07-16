9ab0cc2b87892bdd43db6fdc50ec1ae6139d1538
PhoenixofForce <phoenixofforce@gmail.com>
2017-07-16 16:00:05 +0200

    [+] Better Lamp movement [+] Pickup Lamp in 2.5 Tile range

--- src/de/pof/game/entities/Lamp.java
+++ src/de/pof/game/entities/Lamp.java
@@ -1,5 +1,6 @@
 package de.pof.game.entities;
 
+import de.pof.GUIConstants;
 import de.pof.gamelib.hitboxes.Hitbox;
 import de.pof.gamelib.math.Vec2d;
 import de.pof.textures.TextureHandler;
@@ -15,6 +16,7 @@ public class Lamp implements Entity{
 	private float fuel;
 
 	private boolean blink;
+	private boolean following;
 	private boolean shineBrigth;
 	private long lastUpdate;
 	private long lastShineUpdate;
@@ -25,6 +27,7 @@ public class Lamp implements Entity{
 
 		blink = false;
 		shineBrigth = false;
+		following = true;
 
 		lastUpdate = System.currentTimeMillis();
 		lastShineUpdate = System.currentTimeMillis();
@@ -42,8 +45,6 @@ public class Lamp implements Entity{
 
 	@Override
 	public void update(Map m) {
-		System.out.println(fuel);
-
 		if(fuel <= 100.0f) {
 			if(System.currentTimeMillis() - lastUpdate >= 500) {
 				lastUpdate = System.currentTimeMillis();
@@ -54,6 +55,17 @@ public class Lamp implements Entity{
 		if(shineBrigth && System.currentTimeMillis() - lastShineUpdate >= 500) {
 			fuel -= 10.0f;
 			lastShineUpdate = System.currentTimeMillis();
+		}
+
+		Vec2d tilePos_1 = pos.clone().divide(GUIConstants.TILE_SIZE);
+		Vec2d tilePos_2 = pos.clone().add(0, 2).add(getHitBox().getWidth(), getHitBox().getHeight()).divide(GUIConstants.TILE_SIZE);
+
+		if(!following) {
+			Vec2d vel = new Vec2d(0, 0);
+			if(!(m.isSolid((int) Math.floor(tilePos_2.x), (int) Math.floor(tilePos_2.y)) || m.isSolid((int) Math.floor(tilePos_1.x), (int) Math.floor(tilePos_2.y)))) {
+				vel.y = 2;
+			} else vel.y = 0;
+			this.pos.add(vel);
 		}
 	}
 
@@ -68,13 +80,17 @@ public class Lamp implements Entity{
 
 	@Override
 	public Hitbox getHitBox() {
-		return null;
+		return new Hitbox(Hitbox.HitboxType.RECTANGLE, 0, 0, 17, 20);
 	}
 
 	public void setPosition(double x, double y) {
 		fuel -= pos.distanceTo(new Vec2d(x, y))/30.0f;
 		this.pos.x = x;
 		this.pos.y = y;
+	}
+
+	public void setFollowing(boolean a) {
+		this.following = a;
 	}
 
 	public boolean isShiningBrigth() {
